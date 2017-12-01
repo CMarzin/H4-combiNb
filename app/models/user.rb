1d11da20ef8ef5ffce34de51cec2ba66dd29b0c7
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-01 14:52:53 +0100

    added paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   # presence = field obligatoire
   validates :username, presence: true, uniqueness: {case_senstive: false}, format: {with: /\A[a-zA-Z0-9 _\.]*\z/}
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}, default_url:"/assets/images/default_image.jpg"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/ 
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
