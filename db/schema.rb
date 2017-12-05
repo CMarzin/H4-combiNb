4af5012c15f4e44a347d8b3afb3d0e00c58f890f
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-05 10:07:37 +0100

    [ADD] added avatar field to combis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130143032) do
+ActiveRecord::Schema.define(version: 20171205085917) do
 
   create_table "annonces", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20171130143032) do
     t.string "fueltype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
