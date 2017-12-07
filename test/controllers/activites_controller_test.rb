cd91d6d06f314c9d2c1eb9774fc1144821ccd063
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-07 15:53:34 +0100

    removed tests

--- test/controllers/activites_controller_test.rb
+++ test/controllers/activites_controller_test.rb
@@ -1,48 +1,48 @@
 require 'test_helper'
 
 class ActivitesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @activite = activites(:one)
-  end
+  # setup do
+  #   @activite = activites(:one)
+  # end
 
-  test "should get index" do
-    get activites_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get activites_url
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get new_activite_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get new_activite_url
+  #   assert_response :success
+  # end
 
-  test "should create activite" do
-    assert_difference('Activite.count') do
-      post activites_url, params: { activite: { description: @activite.description, location: @activite.location, titre: @activite.titre } }
-    end
+  # test "should create activite" do
+  #   assert_difference('Activite.count') do
+  #     post activites_url, params: { activite: { description: @activite.description, location: @activite.location, titre: @activite.titre } }
+  #   end
 
-    assert_redirected_to activite_url(Activite.last)
-  end
+  #   assert_redirected_to activite_url(Activite.last)
+  # end
 
-  test "should show activite" do
-    get activite_url(@activite)
-    assert_response :success
-  end
+  # test "should show activite" do
+  #   get activite_url(@activite)
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get edit_activite_url(@activite)
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get edit_activite_url(@activite)
+  #   assert_response :success
+  # end
 
-  test "should update activite" do
-    patch activite_url(@activite), params: { activite: { description: @activite.description, location: @activite.location, titre: @activite.titre } }
-    assert_redirected_to activite_url(@activite)
-  end
+  # test "should update activite" do
+  #   patch activite_url(@activite), params: { activite: { description: @activite.description, location: @activite.location, titre: @activite.titre } }
+  #   assert_redirected_to activite_url(@activite)
+  # end
 
-  test "should destroy activite" do
-    assert_difference('Activite.count', -1) do
-      delete activite_url(@activite)
-    end
+  # test "should destroy activite" do
+  #   assert_difference('Activite.count', -1) do
+  #     delete activite_url(@activite)
+  #   end
 
-    assert_redirected_to activites_url
-  end
+  #   assert_redirected_to activites_url
+  # end
 end
