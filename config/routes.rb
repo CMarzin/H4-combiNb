4f2c4b584ce0bfff732c7554eb710891c614c5ff
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-08 10:20:30 +0100

    started home integration

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   root 'home#index'
   
   get 'home/index'
-
+  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
