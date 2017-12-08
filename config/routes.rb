2fc267a43a79a2ad4b3c09f9868bc47e404c1c7f
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-08 15:58:38 +0100

    page results is kinda working

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   
   get 'home/index'
   get 'pages/index'
+  get 'pages/results'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
