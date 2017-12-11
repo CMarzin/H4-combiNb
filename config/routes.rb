bb970fad92d6580ae172e44fb2de60bf4a96cd84
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-11 17:11:02 +0100

    [MODIFIED] added a link to front

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   get 'home/index'
-  get 'pages/index'
+  get 'pages/index', to: 'pages#index', as: "inte"
   get 'pages/results'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
