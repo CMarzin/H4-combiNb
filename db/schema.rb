3c56a7bdcda1cdf43fc45c07c896d6257e74a32b
H-L <hadrien.lepoutre@gmail.com>
2017-12-17 06:39:46 +0100

    :art: Improving forms 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216231114) do
+ActiveRecord::Schema.define(version: 20171217045039) do
 
   create_table "activites", force: :cascade do |t|
     t.string "titre"
