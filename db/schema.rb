2c9ae4361400442d7e8201f388f281620a118d96
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-17 16:39:41 +0100

    [FIX] fix again

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20171216195813) do
 
-ActiveRecord::Schema.define(version: 20171214084326) do
-  
   create_table "activites", force: :cascade do |t|
     t.string "titre"
     t.text "description"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.string "category"
+    t.string "categorie"
     t.string "image_file_name"
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
-    t.integer "stars_number"
+    t.integer "user_id"
   end
 
   create_table "annonces", force: :cascade do |t|
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 20171214084326) do
     t.datetime "avatar_updated_at"
     t.integer "user_id"
     t.integer "stars_number"
-    t.index ["user_id"], name: "index_combis_on_user_id"
   end
 
   create_table "rates", force: :cascade do |t|
@@ -99,7 +98,6 @@ ActiveRecord::Schema.define(version: 20171214084326) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.integer "stars_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
