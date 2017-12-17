0d32dba31be6942a2579694416cfe5e76cf9bf63
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-17 23:49:24 +0100

    [MODIFIED] front final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,13 @@
 class PagesController < ApplicationController 
   def index
-    render layout: false
     @activites = Activite.all
   end
 
   def results
-    render layout: false
     @activites = Activite.all
   end
 
   def combiList
-    render layout: false
     @combis = Combi.all
   end
 
